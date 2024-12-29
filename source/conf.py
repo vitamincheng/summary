d3fa6f2043d63a2de40ea72bec4af8fbb57bbb4e
Vitamin Cheng <vitmain.cheng@gmail.com>
2024-12-29 19:29:10 +0800

    delete sphinx_rtd_theme and keep html_theme = "sphinx_rtd_theme"

--- source/conf.py
+++ source/conf.py
@@ -14,18 +14,17 @@ release = '0.01'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-#extensions = ['sphinx_copybutton']
+# extensions = ['sphinx_copybutton']
 extensions = []
 templates_path = ['_templates']
 exclude_patterns = []
 
 
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-import sphinx_rtd_theme
+# import sphinx_rtd_theme
 html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # -- Options for PDF output
 latex_engine = 'xelatex'
@@ -36,5 +35,5 @@ latex_elements = {
     'pointsize': '10pt',
 }
 
-#html_theme = 'alabaster'
-#html_static_path = ['_static']
+# html_theme = 'alabaster'
+# html_static_path = ['_static']
